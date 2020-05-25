87852f171854ffe3e4c107836b5d779ae9db78f2
Freddy Ayon <freddyon18@gmail.com>
2020-05-25 10:45:19 -0500

    consumo del api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoService } from './service/info.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba';
+
+  constructor(private service:InfoService){
+  }
+
+  getInfo(){
+    this.service.getInfo();
+  }
 }
---
+++ src/app/service/info.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoService {
+
+  constructor(private http:HttpClient) {
+
+   }
+
+       
+   getInfo(){
+    return this.http.get('https://rickandmortyapi.com/api/character/?page=1');
+  }
+}
